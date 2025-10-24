64bd9346658ba2d07b9616afd7beabad19690b65
saniyack03 <saniyack2003@gmail.com>
2025-10-24 12:25:40 +0530

    completed

---
+++ record14a/circle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import math
+def area(r):
+    return math.pi*r*r
+def perimeter(r):
+    return 2*math.pi*r
+
+
+# In[1]:
+
+
+get_ipython().system('jupyter nbconvert --to python circle.ipynb')
+
+
+# In[ ]:
+
+
+
+
---
+++ record14a/rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def area(l,b):
+    return l*b
+
+def perimeter(l,b):
+    return 2*(l+b)
+    
+
