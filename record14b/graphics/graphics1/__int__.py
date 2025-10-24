2549903cd79afd179d37516d5631beea4eb7f517
saniyack03 <saniyack2003@gmail.com>
2025-10-24 12:26:30 +0530

    completed

---
+++ record14b/graphics/graphics1/__int__.py
@@ -0,0 +1,2 @@
+from. import cuboid
+from. import sphere
---
+++ record14b/graphics/rectangle.py
@@ -0,0 +1,6 @@
+def rectarea(l,b):
+  print("Area of Rectngle:",l*b)
+  
+def rectperi(l,b):
+  print("Perimeter of Rectangle:",2*(l+b))
+  
